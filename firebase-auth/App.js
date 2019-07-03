61330dc96c5ebd4adfcf385c1b47fbb2c426e971
Amruth Pillai <im.amruth@gmail.com>
2019-07-03 10:34:18 +0530

    completed sign in/sign up flow

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View, StatusBar } from 'react-native';
 import SignUpForm from './src/components/SignUpForm';
+import SignInForm from './src/components/SignInForm';
+import { Divider } from 'react-native-elements';
+import firebase from 'firebase'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <SignUpForm />
-    </View>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCn-_SwZVEjoHbn4MP-YPW6nIBPrvFWtK8",
+      authDomain: "advancedreactnative-otp.firebaseapp.com",
+      databaseURL: "https://advancedreactnative-otp.firebaseio.com",
+      projectId: "advancedreactnative-otp",
+      storageBucket: "",
+      messagingSenderId: "822813031290",
+      appId: "1:822813031290:web:3a276b5e4471a27c"
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignUpForm />
+        <Divider />
+        <SignInForm />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'space-around',
     marginTop: StatusBar.currentHeight,
     padding: 20
   },
--- firebase-auth/src/components/SignInForm.js
+++ firebase-auth/src/components/SignInForm.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
+import firebase from 'firebase'
 import axios from 'axios'
 import { Input, Button } from 'react-native-elements'
 
 const ROOT_URL = 'https://us-central1-advancedreactnative-otp.cloudfunctions.net'
 
-export default class SignUpForm extends Component {
-  state = { phone: '' }
+export default class SignInForm extends Component {
+  state = { phone: '', code: '' }
 
   handleSubmit = async () => {
-    const { phone } = this.state
+    const { phone, code } = this.state
     try {
-      await axios.post(`${ROOT_URL}/createUser`, { phone })
-      await axios.post(`${ROOT_URL}/requestOTP`, { phone })
+      const { data } = await axios.post(`${ROOT_URL}/verifyOTP`, { phone, code })
+      firebase.auth().signInWithCustomToken(data.token)
     } catch (error) {
       console.log('An Error Has Occured', error)
     }
@@ -22,13 +22,20 @@ export default class SignUpForm extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.heading}>Signup</Text>
+        <Text style={styles.heading}>Login</Text>
         <View style={{ marginBottom: 30 }}>
           <Input
-            placeholder='+91 123 456 7890'
+            placeholder='9876512345'
             value={this.state.phone}
             label='Please enter your phone number'
             onChangeText={phone => { this.setState({ phone }) }} />
+        </View>
+        <View style={{ marginBottom: 30 }}>
+          <Input
+            placeholder='1234'
+            value={this.state.code}
+            label='Enter your OTP'
+            onChangeText={code => { this.setState({ code }) }} />
         </View>
         <Button
           title='Submit'
