b64c9252e3132498501a06e30f1cb0a15e697c40
Amruth Pillai <im.amruth@gmail.com>
2019-07-03 19:58:02 +0530

    abandoning project as indeed jobs api is closed down

--- job-finder/src/screens/MapScreen.js
+++ job-finder/src/screens/MapScreen.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class MapScreen extends Component {
+  state = {
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    }
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region })
+  }
+
   render() {
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={styles.container}>
+        <MapView
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          region={this.state.region}
+          provider={PROVIDER_GOOGLE}
+          style={styles.container} />
       </View>
     )
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1
   }
 }
